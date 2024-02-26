087d072468f01352a518fe5881eb454ea940450e
ranggahisbu <146620174+ranggahisbu@users.noreply.github.com>
2024-02-26 15:45:50 +0700

    Update luaslingkaran.java

--- luaslingkaran.java
+++ luaslingkaran.java
@@ -6,14 +6,16 @@ public class luaslingkaran {
         Scanner input = new Scanner(System.in);
 
         double phi = 3.14;
-        double luas;
+        double luas,keliling;
 
         System.out.print("Masukkan jari-jari : ");
         double r= input.nextDouble();
 
         luas = phi * r * r;
+        keliling = 2 * phi * r;
 
         System.out.println("Hasil luas lingkarannya adalah = " + luas);
+        System.out.println("Hasil Keliling nya adalah = " + keliling);
 
     }
 }
